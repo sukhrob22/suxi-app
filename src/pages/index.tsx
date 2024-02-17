c4608f3d2cd8f99059ed5b25c11d936b4523af5f
sukhrob22 <ergashevsuxrob986@gmail.com>
2024-02-17 16:19:54 +0500

    server side rendering

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,18 @@
+import { GetServerSideProps } from 'next';
 import Head from 'next/head';
+import { useEffect } from 'react';
 import { Header } from 'src/components';
+import { API_REQUEST } from 'src/services/api.service';
 
-export default function Home() {
+export default function Home(props: HomeProps): JSX.Element {
+    console.log(props);
+
+    // useEffect(() => {
+    //     fetch(API_REQUEST.trending)
+    //         .then((res) => res.json())
+    //         .then((data) => console.log(data));
+    // }, []);
+
     return (
         <div className='relative h-[200vh]'>
             <Head>
@@ -20,4 +31,20 @@ export default function Home() {
             <main></main>
         </div>
     );
+}
+
+export const getServerSideProps: GetServerSideProps<HomeProps> = async () => {
+    const trending = await fetch(API_REQUEST.trending).then((res) =>
+        res.json()
+    );
+
+    return {
+        props: {
+            trending,
+        },
+    };
+};
+
+interface HomeProps {
+    trending: any;
 }
